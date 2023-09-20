f340cb38496014c7fc62f19e0ddc0584117cf763
mmwandera <mmwandera74@gmail.com>
2023-09-20 21:19:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+// Make sure you remove the <main> with id 'main'
+document.querySelector('main').remove()
+
+// Make sure you create an <h1> with id 'victory' with a sweet message in it
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.textContent = 'Mark is the champion innit'
